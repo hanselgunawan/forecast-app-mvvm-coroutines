745211b344a3099be88bd825d158f63d141e095c
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-03-04 00:05:09 -0800

    added layouts and vector assets

--- app/src/main/java/com/hanseltritama/forecastappmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/forecastappmvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hanseltritama.forecastappmvvm
+package com.hanseltritama.forecastappmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hanseltritama.forecastappmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
